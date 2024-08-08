b61985e08a0224262b0dbbc382f92ca179b51bca
nas233 <nas233@gmail.com>
2024-08-08 19:38:31 +0400

    feat: add function in index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let s = Math.abs(n);
+  let res = 0;
+  while (s > 0) {
+    res = res * 10 + s % 10;
+    s = Math.floor( s / 10);
+    console.log(s);
+  }
+
+  return res;
 }
